4dab40c3eb6cfe24f5276ad39dabf8d58b948937
Daniel <danielopyd99@gmail.com>
2021-11-06 14:30:14 +0100

    menu

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu} = require('electron')
 const path = require("path")
+
+
+
 function createWindow(){
     const window = new BrowserWindow({
         width: 500, height: 500,
+        title: "Portal muzyczny",
         webPreferences:{
             preload: path.join(__dirname, 'preload.js'),
             nodeIntegration: true, contextIsolation: false,
@@ -18,9 +22,27 @@ app.whenReady().then(() =>{
             createWindow()
         }
     })
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    Menu.setApplicationMenu(mainMenu);
 })
 app.on('window-all-closed',()=>{
     if(process.platform !== 'darwin'){
         app.quit()
     }
 })
+
+const mainMenuTemplate = [{
+    label: 'Dane',
+    submenu:[{
+        label: "Dodaj wiersz"
+    },
+    {
+        label: "Wyświetl wiersze"
+    },
+    {
+        label : "Wyjdź",click(){
+            app.quit();
+        }
+    }]
+}
+]
