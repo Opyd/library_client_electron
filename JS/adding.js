f3a069f054e51044c5c19618df6e0f94dd3d899d
Opyd <danielopyd99@gmail.com>
2021-11-19 18:03:35 +0100

    Added everything

--- JS/adding.js
+++ JS/adding.js
@@ -1,50 +1,106 @@
 function chooseCategory() {
-    let div = document.getElementById("formContainer");
-    let select = document.getElementById("table");
-    console.log(select.value);
-    if (select.value === "genre") {
-        div.innerHTML = `
+  let div = document.getElementById("formContainer");
+  let select = document.getElementById("table");
+  console.log(select.value);
+  if (select.value === "genre") {
+    div.innerHTML = `
         <input type="text" placeholder="Nazwa gatunku" class="form-control" id='genre' required><br/>
         <button onClick="addGenre()" class="btn btn-primary">Dodaj gatunek</button>
         `;
-    } else {
-        div.innerHTML = `
+  } else if (select.value === "country") {
+    div.innerHTML = `
         <input type="text" placeholder="Nazwa kraju" id='country' class="form-control" required><br/>
         <button onClick="addCountry()" class="btn btn-primary">Dodaj kraj</button>
         `;
-    }
+  } else if (select.value === "publisher") {
+    div.innerHTML = `
+        <input type="text" placeholder="Nazwa wydawcy" id='publisher' class="form-control" required><br/>
+        <button onClick="addPublisher()" class="btn btn-primary">Dodaj wydawcę</button>
+        `;
+  } else if (select.value === "user") {
+    div.innerHTML = `
+        <input type="number" placeholder="ID listy" id='listId' class="form-control" required><br/>
+        <input type="text" placeholder="Login użytkownika" id='userLogin' class="form-control" required><br/>
+        <input type="password" placeholder="Hasło użytkwonika" id='userPassword' class="form-control" required><br/>
+        <button onClick="addUser()" class="btn btn-primary">Dodaj użytkownika</button>
+        `;
+  } else if (select.value === "performer") {
+    div.innerHTML = `
+        <input type="text" placeholder="Nazwa wykonawcy" id='performerName' class="form-control" required><br/>
+        <input type="text" placeholder="Opis wykonawcy" id='performerDesc' class="form-control" required><br/>
+        <button onClick="addPerformer()" class="btn btn-primary">Dodaj wykonawcę</button>
+        `;
+  }
 }
 const { Client } = require("pg");
 const client = new Client({
-    host: "127.0.0.1",
-    port: 5432,
-    database: "baza",
-    user: "user",
-    password: "password",
+  host: "127.0.0.1",
+  port: 5432,
+  database: "baza",
+  user: "user",
+  password: "password",
 });
 client.connect();
 function addGenre() {
-    let val = document.getElementById("genre").value;
-    let addGenreQuery =
-        "INSERT INTO gatunek (nazwa_gatunku) VALUES('" + val + "');";
-    console.log(addGenreQuery);
-    client.query(addGenreQuery, (err, res) => {
-        if (err) {
-            return;
-        }
-        alert("Dodano wiersz");
-    });
+  let val = document.getElementById("genre").value;
+  let addGenreQuery =
+    "INSERT INTO gatunek (nazwa_gatunku) VALUES('" + val + "');";
+  console.log(addGenreQuery);
+  client.query(addGenreQuery, (err, res) => {
+    if (err) {
+      return;
+    }
+    alert("Dodano wiersz");
+  });
 }
 
-function addCountry() {
-    let val = document.getElementById("country").value;
-    let addCountryQuery =
-        "INSERT INTO kraj_pochodzenia (nazwa) VALUES('" + val + "');";
-    console.log(addCountryQuery);
-    client.query(addCountryQuery, (err, res) => {
-        if (err) {
-            return;
-        }
-        alert("Dodano wiersz");
+function addPublisher() {
+  let val = document.getElementById("publisher").value;
+  let addPublisherQuery = `INSERT INTO Wydawca (Nazwa_wydawcy) VALUES ('${val}');`;
+  client.query(addPublisherQuery, (err, res) => {
+    if (err) {
+      return;
+    }
+    alert("Dodano wiersz");
+  });
+}
+
+function addUser() {
+  let listId = document.getElementById("listId").value;
+  let userLogin = document.getElementById("userLogin").value;
+  let userPassword = document.getElementById("userPassword").value;
+
+  let addUserQuery = `INSERT INTO Uzytkownik (ID_listy_uzytkownika, Login_uzytkownika, Haslo_uzytkownika) VALUES ('${listId}','${userLogin}','${userPassword}');`;
+  client.query(addUserQuery, (err, res) => {
+    if (err) return;
+    alert("Dodano wiersz");
+  });
+}
+
+function addPerformer() {
+  let performerName = document.getElementById("performerName").value;
+  let performerDesc = document.getElementById("performerDesc").value;
+  let regex = new RegExp("[a-zA-Z]{2,}");
+  if (!regex.test(performerName)) {
+    alert("Wprowadź poprawne dane");
+  } else {
+    let addPerformerQuery = `INSERT INTO Wykonawca (Nazwa_wykonawcy, Opis) VALUES ('${performerName}','${performerDesc}');`;
+
+    client.query(addPerformerQuery, (err, res) => {
+      if (err) return;
+      alert("Dodano wiersz!");
     });
+  }
+}
+function addCountry() {
+  let val = document.getElementById("country").value;
+  let addCountryQuery =
+    "INSERT INTO kraj_pochodzenia (nazwa) VALUES('" + val + "');";
+  console.log(addCountryQuery);
+  client.query(addCountryQuery, (err, res) => {
+    if (err) {
+      return;
+    }
+    alert("Dodano wiersz");
+  });
 }
